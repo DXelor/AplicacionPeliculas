96e75bff3caef1b6c73c825b33187c91ba43643b
DXelor <dzaliaspro@gmail.com>
2020-12-11 14:35:57 -0500

    Inicio del Proyecto App Peliculas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Text('Hola Mundo desde mi app de Peliculas!'),
+    ));
+  }
+}
