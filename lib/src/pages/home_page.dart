076007b92a642868c5db43f9251cf42c809f5279
DXelor <dzaliaspro@gmail.com>
2020-12-11 22:15:59 -0500

    agregado el flutter-swiper & creado un swiper de targetas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Text('Hola Mundo desde mi app de Peliculas!'),
-    ));
+      appBar: AppBar(
+        title: Text('Peliculas'),
+        backgroundColor: Colors.indigo[600],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperTargetas()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTargetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 15.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        layout: SwiperLayout.STACK,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
+    );
   }
 }
