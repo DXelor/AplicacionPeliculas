9ea37f0cfdeae54a22e76de48194c14e2ab1902e
DXelor <dzaliaspro@gmail.com>
2020-12-17 17:25:43 -0500

    añadido gesture detector y hero animation a las targetas principales (error con el id)

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -16,13 +16,20 @@ class CardSwiper extends StatelessWidget {
         itemWidth: _screenSize.width * 0.6,
         itemHeight: _screenSize.height * 0.5,
         itemBuilder: (BuildContext context, int index) {
-          return ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                image: NetworkImage(peliculas[index].getPosterImg()),
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                fit: BoxFit.cover,
-              ));
+          return Hero(
+            tag: peliculas[index].id,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: GestureDetector(
+                  onTap: () => Navigator.pushNamed(context, 'detalle',
+                      arguments: peliculas[index]),
+                  child: FadeInImage(
+                    image: NetworkImage(peliculas[index].getPosterImg()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                )),
+          );
         },
         itemCount: 3,
         layout: SwiperLayout.STACK,
