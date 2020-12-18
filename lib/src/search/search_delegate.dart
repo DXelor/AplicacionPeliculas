7f6c1130a682e22a4a86f91846df808533b936fb
DXelor <dzaliaspro@gmail.com>
2020-12-18 15:08:03 -0500

    buildActions boton que imprime en consola

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -4,24 +4,30 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     //  implement buildActions : son las acciones del AppBar
-    throw UnimplementedError();
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            print('Click');
+          })
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
+    //implement buildLeading : icono a la izquierda del AppBar
     throw UnimplementedError();
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
+    // implement buildResults : es el builder que da los resultados que se muestran
     throw UnimplementedError();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
+    // implement buildSuggestions: sugerencias de busqueda
     throw UnimplementedError();
   }
 }
